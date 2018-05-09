d97542bade33b09cf6e02fb625632338635507d3
seiju.hattori <seiju1213@gmail.com>
2018-05-09 23:36:06 +0900

    PinBall
    ピンボール

---
+++ Assets/Score.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+    private int toku = 0;
+    private string ten;
+    private bool hit = false;
+    private GameObject score;
+
+	// Use this for initialization
+	void Start () {
+        this.score = GameObject.Find("Score");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ten = toku.ToString();
+        this.score.GetComponent<Text>().text = ten;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        score.tag = other.gameObject.tag;
+           if ( score.tag == "SmallCloudTag")
+            {
+                toku += 15;
+            }
+            else if (score.tag == "LargeStarTag")
+            {
+                toku += 25;
+            }
+            else if (score.tag == "LargeCloudTag")
+            {
+                toku += 20;
+            }
+            else if (score.tag == "SmallStarTag")
+            {
+                toku += 10;
+            }
+        }
+
+   }
---
+++ Assets/To.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class To : MonoBehaviour {
+
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    Touch tch;
+    Vector3 tap;
+	// Use this for initialization
+	void Start () {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defaultAngle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                tap = touch.position;
+            }
+            if (tap.x < 0 && tag == "LeftFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+
+            if (tap.x >= 0 && tag == "RightFripperTag") 
+            {
+                SetAngle(this.flickAngle);
+            }
+        }
+        if (Input.touchCount > 0)
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+}
